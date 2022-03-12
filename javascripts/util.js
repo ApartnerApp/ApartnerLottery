92ecd761b678827bb52c9fe27fbaa123bf5a7bf0
SeanLin <blueeye851114@gmail.com>
2022-03-12 15:54:16 +0800

    Happy birthday!

--- javascripts/util.js
+++
@@ -1,6 +0,0 @@
-function randomInt(min, max) {
-  return min + Math.floor(Math.random() * (max - min + 1))
-}
-Array.prototype.randomItem = function() {
-  return this[randomInt(0, this.length - 1)]
-}
